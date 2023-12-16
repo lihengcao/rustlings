7f2e91bffbd7a8c02865eaa3df841a430be1340b
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2023-12-15 19:02:38 -0500

    iterators!

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,9 +1,19 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn factorial(num: u64) -> u64 {
+    // sol 1
+    // (2..num+1).fold(1, |a, b| a * b)
+
+    // sol 2
+    if num <= 1{
+        1
+    } else {
+        num * factorial(num - 1)
+    }
+
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
